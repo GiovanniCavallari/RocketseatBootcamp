65a6137800f868e5e2081ec5d07f6b1e642b84bb
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-01 22:28:24 -0300

    Frontend tests finished

--- level01/tests/frontend/src/App.js
+++ level01/tests/frontend/src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const result = await api.post('/repositories', {
+      url: 'https://github.com/GiovanniCavallari/RocketseatBootcamp',
+      title: `Bootcamp Rocketseat ${Date.now()}`,
+      techs: ['TS', 'Node', 'React'],
+    });
+
+    const repository = result.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const repoIndex = repositories.findIndex((repo) => repo.id === id);
+    repositories.splice(repoIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
