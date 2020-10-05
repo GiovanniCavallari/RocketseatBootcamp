2f5397cdaaa2851948f76c1e5e1d478c5e04252c
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-05 13:56:05 -0300

    Level02 - backend started

---
+++ level02/backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+
+  return response.json(user);
+});
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333');
+});
