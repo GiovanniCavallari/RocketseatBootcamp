f796319d3a9cc4e2483fe0009603ce0243ab6d01
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-06 19:24:13 -0300

    Level02 - first module finished

--- level02/backend/src/server.ts
+++ level02/backend/src/server.ts
@@ -1,16 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = { name, email };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server running on port 3333');
