05e96ea6fe6dd8c39cf4da391628d1c259381c3c
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-12-16 09:45:29 -0300

    Add files upload

--- GoBarber/backend/src/routes/users.routes.ts
+++ GoBarber/backend/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -12,6 +17,22 @@ usersRouter.post('/', async (request, response) => {
     delete user.password;
 
     return response.status(201).json(user);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatar_filename: request.file.filename,
+    });
+    delete user.password;
+
+    return response.json(user);
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
---
+++ GoBarber/backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { getRepository } from 'typeorm';
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface RequestDto {
+  user_id: string;
+  avatar_filename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatar_filename }: RequestDto): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticate users can update avatar');
+    }
+
+    if (user.avatar) {
+      // Delete previous avatar
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatar_filename;
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
